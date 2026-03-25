31f584bb4d75999df82bb3c18b437be00af22069
alyakinanti <chiiaya729@gmail.com>
2026-03-25 13:52:43 +0700

    menambahkan section "WELCOME"

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function kirimUcapan() {
     alert("Fitur kirim ucapan belum tersedia 😄");
 }
+
+function openInvitation() {
+
+    // contoh efek scroll ke bawah
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth"
+    });
+}
