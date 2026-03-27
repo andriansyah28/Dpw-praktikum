1e5d173efcc95a47198bf439e9fd891129ef655a
alyakinanti <chiiaya729@gmail.com>
2026-03-27 18:18:49 +0700

    menambahkan section "PROFILE", "TIMER" dan "LOKASI"

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ function openInvitation() {
         behavior: "smooth"
     });
 }
+
+//TIMER
+document.addEventListener("DOMContentLoaded", function () {
+    const targetDate = new Date("April 20, 2026 08:00:00").getTime();
+
+    const daysEl = document.querySelector(".days");
+    const hoursEl = document.querySelector(".hours");
+    const minutesEl = document.querySelector(".minutes");
+    const secondsEl = document.querySelector(".seconds");
+
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const distance = targetDate - now;
+
+        if (distance < 0) return;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((distance / (1000 * 60)) % 60);
+        const seconds = Math.floor((distance / 1000) % 60);
+
+        daysEl.innerHTML = days;
+        hoursEl.innerHTML = hours;
+        minutesEl.innerHTML = minutes;
+        secondsEl.innerHTML = seconds;
+    }
+
+    setInterval(updateCountdown, 1000);
+    updateCountdown();
+
+});
